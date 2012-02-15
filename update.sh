384096f16a3a2fd06169420d033b63669c30aa61
Vladislav <klikermellenium@gmail.com>
2012-02-15 19:55:53 +0200

    rewrite update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!sh
 git stash
 git pull origin master
-mysql -uroot -e "drop database denis;"
-mysql -uroot -e "create database denis;"
-mysql -uroot denis < backup/database.sql
-mysql -uroot -e "UPDATE wp_options SET option_value = 'http://denis.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" denis
+mysql -uroot -e "drop database 3dsds;"
+mysql -uroot -e "create database 3dsds;"
+mysql -uroot 3dsds < backup/database.sql
+mysql -uroot -e "UPDATE wp_options SET option_value = 'http://3dsds.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" 3dsds
 git stash pop
 
