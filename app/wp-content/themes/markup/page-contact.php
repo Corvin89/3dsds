900b3f20c2316a898e4c64ab0f3f40a90826f56e
Vladislav <klikermellenium@gmail.com>
2012-02-20 17:02:01 +0200

    pictures for contacts

--- app/wp-content/themes/markup/page-contact.php
+++ app/wp-content/themes/markup/page-contact.php
@@ -6,8 +6,8 @@
     </div>
     <div id="content-internal-center">
         <div class="contacts">
-			<div class="phone"><span class="yellow">Name</span><?php $name=get_post_meta($post->ID,'name'); echo $name[0];?></div>
-			<div class="phone"><span class="yellow">Job</span><?php $job=get_post_meta($post->ID,'job'); echo $job[0];?></div>
+			<div class="name"><span class="yellow">Name</span><?php $name=get_post_meta($post->ID,'name'); echo $name[0];?></div>
+			<div class="job"><span class="yellow">Job</span><?php $job=get_post_meta($post->ID,'job'); echo $job[0];?></div>
             <div class="phone"><span class="yellow">Phone</span><?php $phone=get_post_meta($post->ID,'Telephone'); echo $phone[0];?></div>
             <div class="skype"><span class="yellow">Skype</span><?php $skype=get_post_meta($post->ID,'Skype'); echo $skype[0];?></div>
             <div class="email"><span class="yellow">Email</span><?php $email=get_post_meta($post->ID,'Email'); echo $email[0]; ?></div>
