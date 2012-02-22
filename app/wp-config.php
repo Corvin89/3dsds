1d1c2e1161b774a661e4b2c94c75caa974314a14
Vladislav Fedorischev <klikermellenium@gmail.com>
2012-02-22 23:14:29 +0200

    fixed gitthumb

--- app/wp-config.php
+++ app/wp-config.php
@@ -19,13 +19,13 @@
 define('DB_NAME', '3dsds');
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'admin');
+define('DB_USER', 'root');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', 'admin');
+define('DB_PASSWORD', 'V3ufG0k1c');
 
 /** Имя сервера MySQL */
-define('DB_HOST', '192.168.0.7');
+define('DB_HOST', 'localhost');
 
 /** Кодировка базы данных для создания таблиц. */
 define('DB_CHARSET', 'utf8');
