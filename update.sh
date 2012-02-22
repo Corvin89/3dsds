f75c8a0cc41f7157da23c700b716358b9aaac8dd
Corvin89 <corvin8989@gmail.com>
2012-02-22 17:13:22 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!sh
-git stash
-git pull origin master
-mysql -uroot -e "drop database 3dsds;"
-mysql -uroot -e "create database 3dsds;"
-mysql -uroot 3dsds < backup/database.sql
-mysql -uroot -e "UPDATE wp_options SET option_value = 'http://3dsds.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" 3dsds
-git stash pop
+#git stash
+#git pull origin master
+mysql -uadmin -padmin -e "drop database 3dsds;"
+mysql -uadmin -padmin -e "create database 3dsds;"
+mysql -uadmin -padmin 3dsds < backup/database.sql
+mysql -uadmin -padmin -e "UPDATE wp_options SET option_value = 'http://3dsds.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" 3dsds
+#git stash pop
 
